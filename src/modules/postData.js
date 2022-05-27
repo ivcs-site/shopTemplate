961acb11ac86012fb0dc75daa6486c56f3810907
Mardon <mardonov.mexriddin1997@gmail.com>
2022-05-27 16:04:24 +0500

    save

---
+++ src/modules/postData.js
@@ -0,0 +1,12 @@
+const postData = () => {
+    return fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(cart),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+      .then(res => res.json())
+}
+
+export default postData
